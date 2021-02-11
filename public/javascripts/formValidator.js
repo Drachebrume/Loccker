1e139c5d76bf0d2ea5a59103251c8da2626c95c0
Drachebrume <baptiste.poulain59@gmail.com>
2021-02-11 12:11:23 +0100

    formValidator, port, npm run please

--- public/javascripts/formValidator.js
+++ public/javascripts/formValidator.js
@@ -8,32 +8,49 @@ function validator(valid, inputId) {
     field.classList.add('is-invalid')
   }
 }
-document.getElementById('validationCompany').onchange = function() {
-  let company = document.getElementById('validationCompany');
-  if (company.value.length > 2) {
-    validator(true, "validationCompany");
+document.getElementById('validationUsername').addEventListener("keyup", event => {
+	const name = document.getElementById('validationUsername');
+	const refused = /['`";]/;
+  if (name.value.length > 2 && !(refused.test(name.value))) {
+    validator(true, name.id);
   } else {
-    validator(false, "validationCompany");
+    validator(false, name.id);
   }
-}
-Checker.mail = function() {
-	console.log("test");
-	const mail = document.querySelector('#mail');
-	if (mail.value.includes("@"))
-	{
-		let decompo = mail.value.split("@");
-		if (decompo[1].includes("."))
-		{
-		let alpha = decompo[1].split(".");
-		return true;
+});
+document.getElementById('validationMail').addEventListener("keyup", event => {
+	const mail = document.getElementById('validationMail');
+	const refused = /['`";]/;
+	if (mail.value.includes("@") && !(refused.test(mail.value))) {
+		if ((mail.value.split("@"))[1].includes(".")) {
+			validator(true, mail.id);
+		} else {
+			validator(false, mail.id);
 		}
-		else
-		{
-			return false;
+	} else {
+		validator(false, mail.id);
+	}
+});
+document.getElementById('validationPassword').addEventListener("keyup", event => {
+	const password = document.getElementById('validationPassword');
+	const format = /[ !@#$%^&*()_+\-=\[\]{}:\\|,.<>\/?~]/;
+	const refused = /['`";]/;
+	const numbers = /[1234567890]/
+	if (password.value.length > 7 
+		&& !(refused.test(password.value))
+		&& (/[A-Z]/.test(password.value))
+		&& numbers.test(password.value)
+		&& format.test(password.value)) {
+			validator(true, password.id);
+		} else {
+			validator(false, password.id);
 		}
+});
+document.getElementById('validationPasswordConfirm').addEventListener("keyup", event => {
+	const password1 = document.getElementById('validationPassword');
+	const password2 = document.getElementById('validationPasswordConfirm');
+	if (password1.value === password2.value) {
+		validator(true, password2.id);
+	} else {
+		validator(false, password2.id);
 	}
-	else
-	{
-		return false;
-	}
-};
+})
