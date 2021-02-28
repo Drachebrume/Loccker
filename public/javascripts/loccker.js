697bee10212a423699dec5bcc7b2601869c7b503
Drachebrume <64689165+Drachebrume@users.noreply.github.com>
2021-02-28 04:48:17 +0100

    menu mobile

--- public/javascripts/loccker.js
+++ public/javascripts/loccker.js
@@ -1,4 +1,19 @@
 $(document).ready(() => {
   $('.toast').toast('show');
   $('[data-toggle=popover]').popover();
+  modifyNavbar();
+});
+function modifyNavbar() {
+  console.log("yo");
+  if (window.innerHeight > window.innerWidth) {
+    document.getElementById('mainNavbar').style.display = 'none';
+    document.getElementById('mainNavbarMobile').style.display = 'initial';
+  } else {
+    document.getElementById('mainNavbarMobile').style.display = 'none';
+    document.getElementById('mainNavbar').style.display = 'initial';
+  }
+  
+}
+$(window).resize(() => {
+  modifyNavbar();
 });
